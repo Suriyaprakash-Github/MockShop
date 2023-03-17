b1ca1672e57ea4b8d58fb6cb5a97a87495481059
Suriyaprakash-Github <yoursgump@gmail.com>
2023-03-17 09:35:45 +0530

    fetch more product

--- src/components/Product/ProductItem.js
+++ src/components/Product/ProductItem.js
@@ -10,7 +10,7 @@ const ProductItem = () => {
   useEffect(() => {
     const request = async () => {
       const response = await axios.get(
-        "https://mock.shop/api?query={products(first:%204){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
+        "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
       );
       updatedList(response.data.data.products.edges);
     };
